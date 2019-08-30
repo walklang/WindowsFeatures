cb87800ce0940db1035e234ae1a88af46d4c4c96
suhao <638781@qq.com>
2019-08-30 14:20:38 +0800

    WindowsFeatures: SC_MOVE

---
+++ WindowsFeatures.cpp
@@ -0,0 +1,85 @@
+﻿// WindowsFeatures.cpp : 定义应用程序的入口点。
+//
+
+#include "WindowsFeatures.h"
+#include "framework.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
+
+  auto title = L"Demo";
+  auto window_class = L"DemoWindow";
+
+  WNDCLASSEXW wcex;
+  wcex.cbSize = sizeof(WNDCLASSEX);
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = NULL;
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = NULL;
+  wcex.lpszClassName = window_class;
+  wcex.hIconSm = NULL;
+  RegisterClassExW(&wcex);
+
+  HWND hWnd = CreateWindowW(window_class, title,
+                            WS_POPUP  /*| WS_MINIMIZEBOX | WS_MAXIMIZEBOX*/,
+                            100, 100, 200, 300, nullptr,
+                            nullptr, hInstance, nullptr);
+
+  if (!hWnd) {
+    return FALSE;
+  }
+
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
+
+  MSG msg;
+
+  // 主消息循环:
+  while (GetMessage(&msg, nullptr, 0, 0)) {
+    if (true || !TranslateAccelerator(msg.hwnd, NULL, &msg)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+
+  return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
+                         LPARAM lParam) {
+  switch (message) {
+    case WM_COMMAND: {
+      int wmId = LOWORD(wParam);
+      // 分析菜单选择:
+      switch (wmId) {
+        default:
+          return DefWindowProc(hWnd, message, wParam, lParam);
+      }
+    } break;
+    case WM_PAINT: {
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(hWnd, &ps);
+      EndPaint(hWnd, &ps);
+    } break;
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+    case WM_LBUTTONDOWN:
+      ::SendMessage(hWnd, WM_SYSCOMMAND, SC_MOVE | HTCAPTION, 0);
+      ::SendMessage(hWnd, WM_SYSCOMMAND, SC_MOVE | HTCLIENT, 0);
+      break;
+    default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
+}
